894755cc27c18b00c61d7193c6276cd77a1487fb
monroesmith <monroesmith@simple.com>
2020-08-14 22:08:15 -0700

    Removed hard-coded tasks. They're now rendered dynamically by EJS.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
+const tasks = [
+  {
+    name : 'Math workbook',
+    startTime : '4:00 PM',
+    endTime : '5:00 PM',
+    notes: 'Pages 56-65',
+    tag: 'Math'
+  },
+  {
+    name : 'Read Brave New World',
+    startTime : '3:00 PM',
+    endTime : '4:00 PM',
+    notes: 'Pages 112-145',
+    tag: null
+  },
+  {
+    name : 'Finger paint mural',
+    startTime : '11:00 AM',
+    endTime : '11:30 AM',
+    notes: null,
+    tag: 'Art'
+  }
+]
+
 router.get('/', (req, res) => {
-  res.render('schedule');
+  res.render('schedule', {tasks: tasks});
 });
 
 module.exports = router;
