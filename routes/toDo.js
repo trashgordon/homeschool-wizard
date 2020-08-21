47d8748aa85e8dd675af7085f66f9b80bb9ff5e0
Andrea-Jenkins <64990911+Andrea-Jenkins@users.noreply.github.com>
2020-08-20 18:18:04 -0700

    Added route for toDo

---
+++ routes/toDo.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+// serve toDo page
+router.get('/', (req, res) => {
+  res.render('toDo');
+});
+
+module.exports = router;
+
